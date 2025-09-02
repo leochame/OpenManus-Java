0ff584f301da5b8cc22ccfae0536a5fc749343ee
leochame <leocham.cn@gmail.com>
2025-09-02 19:30:37 +0800

    refactor: Move marketplace and omni agent implementations to test directory

---
+++ src/test/omni/AgentOmniService.java
@@ -0,0 +1,14 @@
+package com.openmanus.agent.impl.omni;
+
+import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
+import dev.langchain4j.service.UserMessage;
+
+@Deprecated
+public interface AgentOmniService {
+
+    @SystemMessage({
+            "You are OpenManus, a large language model trained by OpenManus."
+    })
+    String chat(@MemoryId String conversationId, @UserMessage String userMessage);
+} 
